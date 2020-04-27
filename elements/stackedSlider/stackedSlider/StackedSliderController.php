d0d92704cae0ec4cb071353a1f81a42d7c375012
Camila Lacerda <milacerda@gmail.com>
2020-04-27 01:13:25 -0300

    php

--- elements/stackedSlider/stackedSlider/StackedSliderController.php
+++ elements/stackedSlider/stackedSlider/StackedSliderController.php
@@ -31,12 +31,6 @@ class StackedSliderController extends Container implements Module
                 'template_redirect',
                 'checkPlugin'
             );
-            // $this->addFilter(
-            //     'vcv:ajaxForm:render:response', 
-            //     'renderForm'
-            // );
-
-            $this->wpAddAction( 'wp_enqueue_scripts', 'include_react_files' );
 
             define('VCV_WP_SS_CONTROLLER', true);
         }
@@ -52,7 +46,6 @@ class StackedSliderController extends Container implements Module
     {
         $var = new PostType();
         $post_types = $var->getPostTypes();
-        
 
         $posts = get_posts('post_type=post&numberposts=3');
 
@@ -63,59 +56,30 @@ class StackedSliderController extends Container implements Module
             'value' => $post_types,
         ];
 
-        // error_log(print_r($payload, true));
-        // error_log($variables['$postType']);
         return $variables;
     }
 
     protected function checkPlugin()
     {
-        // if (!defined('WPCF7_VERSION')) {
-            add_shortcode(
-                'stackedSlider',
-                function () {
-                    return __('Plugin is not activated', 'vcwb');
-                }
-            );
-        // }
+
+        add_shortcode(
+            'stackedSlider',
+            function () {
+                return __('Plugin is not activated', 'vcwb');
+            }
+        );
+
     }
 
     protected function renderForm($response, $payload, WpWidgets $widgets)
     {
-        // error_log(print_r($response, true));
-        // error_log(print_r($payload, true));
-        // error_log(print_r($widgets, true));
         if ($payload['action'] === 'vcv:stackedSlider:form') {
             $element = $payload['element'];
             $postType = $element['sourceType'];
 
-            // $posts = get_posts('post_type='.$postType.'&numberposts=3');
-
             $this->getVariables(['$postType' => $postType], $payload);
-
-            // $variables[] = [
-            //     'key' => 'vcvWpStackedSliderSource',
-            //     'value' => $posts,
-            // ];
-
-            // $response['html'] = $posts;
         }
         return $response;
-    }
-
-    function include_react_files() {
-        // wp_enqueue_style( 'prefix-style', plugins_url('css/main.ae114d0c.css', __FILE__) );
-
-        // add the JS file to the footer - true as the last parameter
-        wp_register_script( 'plugin-scripts', plugins_url('../public/dist/element.bundle.js',
-         __FILE__),array(),  '26.0', true );
-        $data = array( 
-                'key1' => 'value1',
-                'key2' => 'value2'
-            );
-
-        wp_localize_script( 'plugin-scripts', 'object', $data );
-        wp_enqueue_script( 'plugin-scripts' );
     }
 
 }
